530b78f68647eb06c0ee100b93f3af76bc311917
ruksadsiddiqui <v-rsiddiqui>
2018-04-13 10:29:12 +0530

    fetching data on Ui and url submit reactive form

--- from-end-wpa/src/app/app.component.ts
+++ from-end-wpa/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Ng2SmartTableModule,LocalDataSource } from 'ng2-smart-table';
 import { NewsFeederService } from './services/news-feeder.service';
 import { FormControl, FormGroup } from '@angular/forms';
+import { RssFeed } from './model/rss-feed';
 
 @Component({
   selector: 'app-root',
@@ -14,9 +15,11 @@ export class AppComponent implements OnInit {
   title = 'RSS feed Reader';
   source: LocalDataSource;
   submitRssFeed:FormGroup;
-
+  actualData:any[]
+ // dataModified:RssFeed[]=new Array();
+  
   constructor(private newsFeederService:NewsFeederService) {
-    this.source = new LocalDataSource(this.data); // create the source
+    // create the source
   }
 
   ngOnInit(): void {
@@ -37,42 +40,54 @@ export class AppComponent implements OnInit {
   fetchAllNewsFeedAndMapToUIModel(url:string){
     this.newsFeederService.getAllRssFeeds(url).subscribe(
       res=>{
+        this.actualData=res;
         console.log(res);
         
+      },(error)=>{
+
+      },()=>{
+        this.source = new LocalDataSource(this.actualData); 
       }
     )
   }
 
+  transformData(){
+
+  }
+  
   settings = {
     columns: {
       title: {
         title: 'Title'
       },
-      author: {
+      authors: {
         title: 'Author'
       },
       link: {
         title: 'Link'
       },
       contents: {
-        title: 'Contents'
+        title: 'Contents',
+        width:'60px',
+        type:'html'
       }
     }
   };
 
-  data = [
-    {
-      title: 1,
-      author: "Leanne Graham",
-      link: "Bret",
-      contents: "Sincere@april.biz"
-    },
-    // ... other rows here
-    {
-      title: 11,
-      author: "Nicholas DuBuque",
-      link: "Nicholas.Stanton",
-      contents: "Rey.Padberg@rosamond.biz"
-    }
-  ];
-}
+//   data = [
+//     {
+//       title: 1,
+//       author: "Leanne Graham",
+//       link: "Bret",
+//       contents: "Sincere@april.biz"
+//     },
+//     // ... other rows here
+//     {
+//       title: 11,
+//       author: "Nicholas DuBuque",
+//       link: "Nicholas.Stanton",
+//       contents: "Rey.Padberg@rosamond.biz"
+//     }
+//   ];
+
+ }
